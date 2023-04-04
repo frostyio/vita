eb4bdeadf9cb389ba7f9b379836b1e90f7c227d0
frosty <57603739+frostyio@users.noreply.github.com>
2023-04-03 18:43:19 -0600

    sync

--- crates/bytecode/src/lua51/mod.rs
+++ crates/bytecode/src/lua51/mod.rs
@@ -1,2 +1,40 @@
 mod common;
+use crate::shared::{luatests, out_dir};
+use anyhow::Result;
 pub use common::inst;
+use std::{
+	io::Write,
+	process::{Command, Stdio},
+};
+
+pub fn compile(script_name: &str) -> Result<String> {
+	let script = luatests().join(script_name);
+	let output = Command::new("/home/frosty/Documents/lua-5.1.5/src/luac")
+		.arg("-o")
+		.arg("-")
+		.arg(script)
+		.output()?;
+
+	Ok(String::from_utf8_lossy(&output.stdout).to_string())
+}
+
+pub fn hexyl(data: &str) -> Result<String> {
+	let mut proc = Command::new("hexyl")
+		.arg("-n")
+		.arg(data.len().to_string())
+		.stdin(Stdio::piped())
+		.stdout(Stdio::piped())
+		.spawn()?;
+
+	proc.stdin.as_mut().unwrap().write_all(data.as_bytes())?;
+
+	let output = proc.wait_with_output()?;
+	Ok(String::from_utf8_lossy(&output.stdout).to_string())
+}
+
+#[test]
+fn test_compile() {
+	let compiled = compile("numerical_for.lua").unwrap();
+	// hexyl(&compiled);
+	println!("{}", hexyl(&compiled).unwrap());
+}
--- crates/bytecode/src/shared/mod.rs
+++ crates/bytecode/src/shared/mod.rs
@@ -1,1 +1,16 @@
 pub mod macros;
+use std::path::PathBuf;
+
+pub fn out_dir() -> PathBuf {
+	let mut dir = std::env::current_dir().unwrap();
+	dir.pop();
+	dir.pop();
+	dir.join("out")
+}
+
+pub fn luatests() -> PathBuf {
+	let mut dir = std::env::current_dir().unwrap();
+	dir.pop();
+	dir.pop();
+	dir.join("luatests")
+}
